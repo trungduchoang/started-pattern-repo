12193d347f1677f279644d557187a906f0c2b6d5
t-hoang-cna <t-hoang-cna@usen-next.jp>
2020-08-19 17:45:01 +0700

    Add Sample Page

--- src/pages/Sample/index.jsx
+++ src/pages/Sample/index.jsx
@@ -1,5 +1,51 @@
 import React from "react";
+import PhotoContextProvider from "./context/PhotoContext";
+import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
+import Header from "./components/Header";
+import Item from "./components/Item";
+import Search from "./components/Search";
+import NotFound from "./components/NotFound";
 
-const Sample = () => <></>;
+const Sample = () => {
+  // Prevent page reload, clear input, set URL and push history on submit
+  const handleSubmit = (e, history, searchInput) => {
+    e.preventDefault();
+    e.currentTarget.reset();
+    const url = `/search/${searchInput}`;
+    history.push(url);
+  };
+
+  return (
+    <PhotoContextProvider>
+      <HashRouter basename="/SnapScout">
+        <div className="container">
+          <Route
+            render={props => (
+              <Header handleSubmit={handleSubmit} history={props.history} />
+            )}
+          />
+          <Switch>
+            <Route exact path="/" render={() => <Redirect to="/mountain" />} />
+
+            <Route
+              path="/mountain"
+              render={() => <Item searchTerm="mountain" />}
+            />
+            <Route path="/beach" render={() => <Item searchTerm="beach" />} />
+            <Route path="/bird" render={() => <Item searchTerm="bird" />} />
+            <Route path="/food" render={() => <Item searchTerm="food" />} />
+            <Route
+              path="/search/:searchInput"
+              render={props => (
+                <Search searchTerm={props.match.params.searchInput} />
+              )}
+            />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </HashRouter>
+    </PhotoContextProvider>
+  );
+};
 
 export default Sample;
